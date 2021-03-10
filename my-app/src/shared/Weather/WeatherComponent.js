f46567a4bdd51efb1be09e3b04186f2fcd88c66c
Alexandru Pop <alexandru.pop@3pillarglobal.com>
2021-03-10 15:10:05 +0200

    inca niste modificari

--- my-app/src/shared/Weather/WeatherComponent.js
+++ my-app/src/shared/Weather/WeatherComponent.js
@@ -23,6 +23,7 @@ const WeatherComponent = ({ model }) => {
       <div>Feels like: {kelvinToCelsius(feelTemp)}</div>
       <div>Max: {kelvinToCelsius(max)}</div>
       <div>Max: {kelvinToCelsius(min)}</div>
+      <p>MAi adaugaam niste modificari si pe aici ca sa vedem cum merge treaaba</p>
     </div>
   );
 };
